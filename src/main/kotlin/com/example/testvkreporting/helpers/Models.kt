b9e4edac37e59a33968dc6df8b77cbf5383ff995
Daniil Roman <danroman17397@gmail.com>
2023-05-01 22:18:59 +0200

    Fix NPE

--- src/main/kotlin/com/example/testvkreporting/helpers/Models.kt
+++ src/main/kotlin/com/example/testvkreporting/helpers/Models.kt
@@ -1,13 +1,12 @@
 package com.example.testvkreporting.helpers
 
 import java.io.File
-import java.lang.NullPointerException
 import java.sql.ResultSet
 
 data class MemeImage(val id: String, val image: ByteArray, val published: String) {
     companion object {
         fun create(rs: ResultSet): MemeImage {
-            val published = try { rs.getString("published") } catch (ex: NullPointerException) { "" }
+            val published = rs.getString("published") ?: ""
             return MemeImage(
                 rs.getString("file_id"),
                 rs.getBytes("file"),
